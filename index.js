53386b8e86c7c6326222cd9e914101846af4601c
iambryanhaney <i.am.bryan.haney@gmail.com>
2020-06-03 22:44:12 -0500

    Done.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key) {
-    const newDriver = { ...driver }
+    const newDriver = Object.assign({}, driver)
     delete newDriver[key]
     return newDriver
 }
